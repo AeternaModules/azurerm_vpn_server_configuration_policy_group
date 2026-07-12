2a2154dfef53a9a836252bc06ebc1f8f977b1be9
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:31 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "vpn_server_configuration_policy_groups" {
-  description = "All vpn_server_configuration_policy_group resources"
-  value       = azurerm_vpn_server_configuration_policy_group.vpn_server_configuration_policy_groups
-}
 output "vpn_server_configuration_policy_groups_is_default" {
-  description = "List of is_default values across all vpn_server_configuration_policy_groups"
-  value       = [for k, v in azurerm_vpn_server_configuration_policy_group.vpn_server_configuration_policy_groups : v.is_default]
+  description = "Map of is_default values across all vpn_server_configuration_policy_groups, keyed the same as var.vpn_server_configuration_policy_groups"
+  value       = { for k, v in azurerm_vpn_server_configuration_policy_group.vpn_server_configuration_policy_groups : k => v.is_default }
 }
 output "vpn_server_configuration_policy_groups_name" {
-  description = "List of name values across all vpn_server_configuration_policy_groups"
-  value       = [for k, v in azurerm_vpn_server_configuration_policy_group.vpn_server_configuration_policy_groups : v.name]
+  description = "Map of name values across all vpn_server_configuration_policy_groups, keyed the same as var.vpn_server_configuration_policy_groups"
+  value       = { for k, v in azurerm_vpn_server_configuration_policy_group.vpn_server_configuration_policy_groups : k => v.name }
 }
 output "vpn_server_configuration_policy_groups_policy" {
-  description = "List of policy values across all vpn_server_configuration_policy_groups"
-  value       = [for k, v in azurerm_vpn_server_configuration_policy_group.vpn_server_configuration_policy_groups : v.policy]
+  description = "Map of policy values across all vpn_server_configuration_policy_groups, keyed the same as var.vpn_server_configuration_policy_groups"
+  value       = { for k, v in azurerm_vpn_server_configuration_policy_group.vpn_server_configuration_policy_groups : k => v.policy }
 }
 output "vpn_server_configuration_policy_groups_priority" {
-  description = "List of priority values across all vpn_server_configuration_policy_groups"
-  value       = [for k, v in azurerm_vpn_server_configuration_policy_group.vpn_server_configuration_policy_groups : v.priority]
+  description = "Map of priority values across all vpn_server_configuration_policy_groups, keyed the same as var.vpn_server_configuration_policy_groups"
+  value       = { for k, v in azurerm_vpn_server_configuration_policy_group.vpn_server_configuration_policy_groups : k => v.priority }
 }
 output "vpn_server_configuration_policy_groups_vpn_server_configuration_id" {
-  description = "List of vpn_server_configuration_id values across all vpn_server_configuration_policy_groups"
-  value       = [for k, v in azurerm_vpn_server_configuration_policy_group.vpn_server_configuration_policy_groups : v.vpn_server_configuration_id]
+  description = "Map of vpn_server_configuration_id values across all vpn_server_configuration_policy_groups, keyed the same as var.vpn_server_configuration_policy_groups"
+  value       = { for k, v in azurerm_vpn_server_configuration_policy_group.vpn_server_configuration_policy_groups : k => v.vpn_server_configuration_id }
 }
 
